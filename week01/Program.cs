a6796e570c826743d59e733be3071c38094152c7
W-Weir <s3831617@student.rmit.edu.au>
2020-02-25 16:27:08 +1100

    First commit

--- week01/Program.cs
+++ week01/Program.cs
@@ -6,7 +6,15 @@ namespace week01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your first name?");
+            string firstname = Console.ReadLine();
+            
+            Console.WriteLine("What's your surname?");
+            string surname = Console.ReadLine();
+            
+            string message = "G'day, " + firstname + " " + surname + "!";
+
+            Console.WriteLine(message);
         }
     }
 }
