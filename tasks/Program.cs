a8730beed15df2422f359079e00ec0ffa568d48e
101796559 <101796559@student.swin.edu.au>
2020-02-25 20:38:30 +1100

    Started Guess the Number Challenge

---
+++ tasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minimumResult = 1;
+            int maximumResult = 10;
+
+            Console.WriteLine("Welcome to the number guessing game.");
+            Console.WriteLine("Guess a number between " + minimumResult.ToString() + " and " + maximumResult.ToString());
+
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            while (guess != maximumResult) {
+                if (guess < maximumResult)
+                {
+                    Console.WriteLine("Nope. Higher");
+                }
+                else if (guess > maximumResult)
+                {
+                    Console.WriteLine("Nope. Lower");
+                }
+                Console.WriteLine("Choose another number between " + minimumResult.ToString() + " and " + maximumResult.ToString());
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Nice guess!");
+                
+        }
+    }
+}
