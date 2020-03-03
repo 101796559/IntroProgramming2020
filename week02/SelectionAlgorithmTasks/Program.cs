a0b443c0fb3e3dfe596ae1a30985386b216f208a
101796559 <101796559@student.swin.edu.au>
2020-03-03 16:36:10 +1100

    week02 work

---
+++ week02/SelectionAlgorithmTasks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SelectionAlgorithmTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A program receives two numbers. 
+            // If the numbers add up to greater than 10, output "Sum of numbers is greater than 10". 
+            // If the numbers add up to less than 10, output "Sum of numbers is less than 10". 
+            // What happens if the numbers add up to 10?
+
+            // Get numbers a, b
+            // If (a + b) > 10
+                // print "Sum is greater than 10"
+            // ELSE IF (a + b) == 10
+                // print "Sum is 10"
+            // ELSE 
+                // print "Sum is less than 10"
+        }
+    }
+}
---
+++ week02/SelectionTask/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get username and password
+
+            // If username exists 
+                    // If password correct
+                        // print login successful
+                    // Else
+                        // Print login unsuccessful
+            // Else
+                // print login unsuccessful
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            string sysUsername = "bob";
+            string sysPassword = "bob";
+
+            if (username == sysUsername) {
+                // Uncomment to check username first.
+                // Console.Write("Password: ");
+                // string password = Console.ReadLine();
+
+                if (password == sysPassword) {
+                    System.Console.WriteLine("Login Successful.");
+                }
+                else {
+                    System.Console.WriteLine("Login Failed.");
+                }
+            }
+            else {
+                System.Console.WriteLine("Login Failed.");
+            }
+
+            
+        }
+    }
+}
