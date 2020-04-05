3012b39afc3fccf9242062a629b6620c86e5102b
101796559 <101796559@student.swin.edu.au>
2020-04-05 15:39:34 +1000

    Exercise 1 - Task 2

--- NestedRepetition/Task1/Program.cs
+++ NestedRepetition/Task1/Program.cs
@@ -5,21 +5,43 @@ namespace NestedRepetition
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // A program receives a user's name and prints it for the amount of times that the user specifies.
             System.Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
 
-            int counter = 0;
-            while (counter < 10) {
-                int counter2 = 0;
-
-                while(counter2 < 10) {
-                    System.Console.Write($"{userName} ");
-                    counter2 += 1;
+            int repeats = 10; 
+            int defaultRepeats = repeats;
+            int attempts = 0;
+            bool valid = false;
+            while (attempts < 3 && !valid) {
+                System.Console.Write($"Enter number of times to repeat (default {defaultRepeats}): ");
+                string response = Console.ReadLine();
+                if (response != "") {
+                    if (!int.TryParse(response, out repeats)) {
+                        System.Console.WriteLine("Value entered must be an integer.");
+                        attempts += 1;
+                    }
+                    else {
+                        valid = true;
+                    }
                 }
+                else {
+                    valid = true;
+                }
+            }
 
-                System.Console.WriteLine();
+            int counter = 0;
+            while (counter < repeats) {
+                System.Console.Write($"{userName} ");
                 counter += 1;
+            }
+
+            if (attempts > 2) {
+                System.Console.WriteLine("Too many attempts.");
+            }
+            else {
+                System.Console.WriteLine();
             }
         }
     }
