81baf2335aa18fbd4e4732daaefe793924b10f8b
101796559 <101796559@student.swin.edu.au>
2020-04-05 15:53:38 +1000

    Exercise 2 - Task 2

--- NestedRepetition/Exercise2/Program.cs
+++ NestedRepetition/Exercise2/Program.cs
@@ -6,12 +6,33 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            // Task 1
+            // Task 2
             int counter = 0;
-            while (counter < 5) {
+            int lines = 10;
+            int defaultLines = lines;
+
+            while (counter < 3) {
+                System.Console.Write($"Enter number of lines to display (default {defaultLines}): ");
+                string response = Console.ReadLine();
+                if (response != "") {
+                    if (!int.TryParse(response, out lines)) {
+                        System.Console.WriteLine("Enter an integer.");
+                        counter += 1;
+                    }
+                    else {
+                        counter = 10; // breaks loop without needing an additional variable.
+                    }
+                }
+                else {
+                    counter = 10;
+                }
+            }
+
+            counter = 0;
+            while (counter < lines) {
                 int counter2 = counter;
 
-                while (counter2 < 5) {
+                while (counter2 < lines) {
                     System.Console.Write("x");
                     counter2 += 1;
                 }
