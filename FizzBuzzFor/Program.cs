17946d197cf76496c257e705637d6b26fb06e556
101796559 <101796559@student.swin.edu.au>
2020-04-15 14:53:23 +1000

    Restructured Folders

---
+++ FizzBuzzFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 3;
+            int buzz = 5;
+
+            for (int i = 1; i <= 100; i++) {
+                if (i % fizz == 0 && i % buzz == 0) {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                else if (i % fizz == 0) {
+                    System.Console.WriteLine("Fizz");
+                }
+                else if (i % buzz == 0) {
+                    System.Console.WriteLine("Buzz");
+                }
+                else {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
