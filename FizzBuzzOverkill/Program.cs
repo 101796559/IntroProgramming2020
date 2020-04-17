30e574090bf37b749b63d502697e87707a89152e
101796559 <101796559@student.swin.edu.au>
2020-04-17 15:51:54 +1000

    Tried to over-engineer fizzbuzz

---
+++ FizzBuzzOverkill/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>() {
+                new Item(3, "fizz"),
+                new Item(5, "buzz"),
+                new Item(7, "beep")
+            };
+            fizzbuzz(items);
+        }
+
+        static void fizzbuzz(List<Item> items) {
+            for (int i = 1; i <= 100; i++) {
+                string message = "";
+                foreach (Item item in items) {
+                    if (i % item.number == 0) message += item.word;
+                }
+                if (message == "") message += i.ToString();
+
+                System.Console.WriteLine(message);
+            };
+        }
+    }
+
+    class Item {
+        public int number;
+        public string word;
+        public Item(int number, string word) {
+            this.number = number;
+            this.word = word;
+        }
+    }
+}
