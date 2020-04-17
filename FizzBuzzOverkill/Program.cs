1e5f25bfcaaf0ee7175bf0357ba46e2358e2b547
101796559 <101796559@student.swin.edu.au>
2020-04-17 16:07:07 +1000

    Little cleaner

--- FizzBuzzOverkill/Program.cs
+++ FizzBuzzOverkill/Program.cs
@@ -2,26 +2,21 @@
 
 namespace fizzbuzz
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Item> items = new List<Item>() {
-                new Item(3, "fizz"),
-                new Item(5, "buzz"),
-                new Item(7, "beep")
+    class Program {
+        static void Main(string[] args) {
+            List<Item> items = new List<Item>() { 
+                new Item(3, "fizz"), 
+                new Item(5, "buzz"), 
             };
-            fizzbuzz(items);
-        }
-
-        static void fizzbuzz(List<Item> items) {
+        
             for (int i = 1; i <= 100; i++) {
                 string message = "";
                 foreach (Item item in items) {
                     if (i % item.number == 0) message += item.word;
                 }
-                if (message == "") message += i.ToString();
-
+                if (message == "") {
+                    message = i.ToString();
+                }
                 System.Console.WriteLine(message);
             };
         }
